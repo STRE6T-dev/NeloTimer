f322a579701d3866b650a7c3af32c8c49fdaec4e
STRE6T <STRE6T@STRE6T-DESKTOP>
2021-11-02 09:56:25 +0200

    Add project files.

---
+++ NeloTimer/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿
+namespace NeloTimer
+{
+    partial class frmNeloFoceser
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDeathmatch = new System.Windows.Forms.Label();
+            this.lblAimLabs = new System.Windows.Forms.Label();
+            this.lblStudy = new System.Windows.Forms.Label();
+            this.lblBreak = new System.Windows.Forms.Label();
+            this.btDeathMatch = new System.Windows.Forms.Button();
+            this.btAimLabs = new System.Windows.Forms.Button();
+            this.btStudy = new System.Windows.Forms.Button();
+            this.btBreak = new System.Windows.Forms.Button();
+            this.btnDeathMatch = new System.Windows.Forms.TextBox();
+            this.btnAimLab = new System.Windows.Forms.TextBox();
+            this.btnBreak = new System.Windows.Forms.TextBox();
+            this.btnStudy = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // lblDeathmatch
+            // 
+            this.lblDeathmatch.AutoSize = true;
+            this.lblDeathmatch.Location = new System.Drawing.Point(223, 9);
+            this.lblDeathmatch.Name = "lblDeathmatch";
+            this.lblDeathmatch.Size = new System.Drawing.Size(75, 15);
+            this.lblDeathmatch.TabIndex = 0;
+            this.lblDeathmatch.Text = "Death Match";
+            // 
+            // lblAimLabs
+            // 
+            this.lblAimLabs.AutoSize = true;
+            this.lblAimLabs.Location = new System.Drawing.Point(424, 9);
+            this.lblAimLabs.Name = "lblAimLabs";
+            this.lblAimLabs.Size = new System.Drawing.Size(51, 15);
+            this.lblAimLabs.TabIndex = 1;
+            this.lblAimLabs.Text = "Aim Lab";
+            // 
+            // lblStudy
+            // 
+            this.lblStudy.AutoSize = true;
+            this.lblStudy.Location = new System.Drawing.Point(234, 126);
+            this.lblStudy.Name = "lblStudy";
+            this.lblStudy.Size = new System.Drawing.Size(37, 15);
+            this.lblStudy.TabIndex = 2;
+            this.lblStudy.Text = "Study";
+            // 
+            // lblBreak
+            // 
+            this.lblBreak.AutoSize = true;
+            this.lblBreak.Location = new System.Drawing.Point(400, 126);
+            this.lblBreak.Name = "lblBreak";
+            this.lblBreak.Size = new System.Drawing.Size(75, 15);
+            this.lblBreak.TabIndex = 3;
+            this.lblBreak.Text = "Break 10 min";
+            // 
+            // btDeathMatch
+            // 
+            this.btDeathMatch.Location = new System.Drawing.Point(187, 68);
+            this.btDeathMatch.Name = "btDeathMatch";
+            this.btDeathMatch.Size = new System.Drawing.Size(138, 23);
+            this.btDeathMatch.TabIndex = 4;
+            this.btDeathMatch.Text = "Start Death Match";
+            this.btDeathMatch.UseVisualStyleBackColor = true;
+            this.btDeathMatch.Click += new System.EventHandler(this.btDeathMatch_Click);
+            // 
+            // btAimLabs
+            // 
+            this.btAimLabs.Location = new System.Drawing.Point(367, 68);
+            this.btAimLabs.Name = "btAimLabs";
+            this.btAimLabs.Size = new System.Drawing.Size(157, 23);
+            this.btAimLabs.TabIndex = 5;
+            this.btAimLabs.Text = "Start Aim Labs";
+            this.btAimLabs.UseVisualStyleBackColor = true;
+            this.btAimLabs.Click += new System.EventHandler(this.btAimLabs_Click);
+            // 
+            // btStudy
+            // 
+            this.btStudy.Location = new System.Drawing.Point(187, 188);
+            this.btStudy.Name = "btStudy";
+            this.btStudy.Size = new System.Drawing.Size(138, 23);
+            this.btStudy.TabIndex = 6;
+            this.btStudy.Text = "Start Study";
+            this.btStudy.UseVisualStyleBackColor = true;
+            this.btStudy.Click += new System.EventHandler(this.btStudy_Click);
+            // 
+            // btBreak
+            // 
+            this.btBreak.Location = new System.Drawing.Point(367, 188);
+            this.btBreak.Name = "btBreak";
+            this.btBreak.Size = new System.Drawing.Size(157, 23);
+            this.btBreak.TabIndex = 7;
+            this.btBreak.Text = "Start Break";
+            this.btBreak.UseVisualStyleBackColor = true;
+            this.btBreak.Click += new System.EventHandler(this.btBreak_Click);
+            // 
+            // btnDeathMatch
+            // 
+            this.btnDeathMatch.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnDeathMatch.Location = new System.Drawing.Point(187, 33);
+            this.btnDeathMatch.Name = "btnDeathMatch";
+            this.btnDeathMatch.ReadOnly = true;
+            this.btnDeathMatch.Size = new System.Drawing.Size(138, 29);
+            this.btnDeathMatch.TabIndex = 8;
+            this.btnDeathMatch.Text = "00:00:00";
+            this.btnDeathMatch.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnAimLab
+            // 
+            this.btnAimLab.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnAimLab.Location = new System.Drawing.Point(367, 33);
+            this.btnAimLab.Name = "btnAimLab";
+            this.btnAimLab.ReadOnly = true;
+            this.btnAimLab.Size = new System.Drawing.Size(157, 29);
+            this.btnAimLab.TabIndex = 9;
+            this.btnAimLab.Text = "00:00:00";
+            this.btnAimLab.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnBreak
+            // 
+            this.btnBreak.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnBreak.Location = new System.Drawing.Point(367, 153);
+            this.btnBreak.Name = "btnBreak";
+            this.btnBreak.ReadOnly = true;
+            this.btnBreak.Size = new System.Drawing.Size(157, 29);
+            this.btnBreak.TabIndex = 10;
+            this.btnBreak.Text = "00:00:00";
+            this.btnBreak.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnStudy
+            // 
+            this.btnStudy.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.btnStudy.Location = new System.Drawing.Point(187, 153);
+            this.btnStudy.Name = "btnStudy";
+            this.btnStudy.ReadOnly = true;
+            this.btnStudy.Size = new System.Drawing.Size(138, 29);
+            this.btnStudy.TabIndex = 11;
+            this.btnStudy.Text = "00:00:00";
+            this.btnStudy.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // frmNeloFoceser
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnStudy);
+            this.Controls.Add(this.btnBreak);
+            this.Controls.Add(this.btnAimLab);
+            this.Controls.Add(this.btnDeathMatch);
+            this.Controls.Add(this.btBreak);
+            this.Controls.Add(this.btStudy);
+            this.Controls.Add(this.btAimLabs);
+            this.Controls.Add(this.btDeathMatch);
+            this.Controls.Add(this.lblBreak);
+            this.Controls.Add(this.lblStudy);
+            this.Controls.Add(this.lblAimLabs);
+            this.Controls.Add(this.lblDeathmatch);
+            this.Name = "frmNeloFoceser";
+            this.Text = "Nelo Focus";
+            this.Load += new System.EventHandler(this.frmNeloFoceser_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDeathmatch;
+        private System.Windows.Forms.Label lblAimLabs;
+        private System.Windows.Forms.Label lblStudy;
+        private System.Windows.Forms.Label lblBreak;
+        private System.Windows.Forms.Button btDeathMatch;
+        private System.Windows.Forms.Button btAimLabs;
+        private System.Windows.Forms.Button btStudy;
+        private System.Windows.Forms.Button btBreak;
+        private System.Windows.Forms.TextBox btnDeathMatch;
+        private System.Windows.Forms.TextBox btnAimLab;
+        private System.Windows.Forms.TextBox btnBreak;
+        private System.Windows.Forms.TextBox btnStudy;
+    }
+}
+
---
+++ NeloTimer/Form1.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace NeloTimer
+{
+    public partial class frmNeloFoceser : Form
+    {
+        System.Timers.Timer tDeath,tAim,tStudy,tBreak;
+        String chooice;
+        Boolean deathClick = false;
+        Boolean aimClick = false;
+        Boolean studyClick = false;
+        Boolean breakClick = false;
+        int h, m, s;
+        public frmNeloFoceser()
+        {
+           
+            InitializeComponent();
+            
+        }
+
+        private void btDeathMatch_Click(object sender, EventArgs e)
+        {
+            deathClick = true;
+            aimClick = false;
+            tAim.Stop();
+            studyClick = false;
+            tStudy.Stop();
+            breakClick = false;
+            tBreak.Stop();
+
+            h = 0;
+            m = 0;
+            s = 0;
+            chooice = btnDeathMatch.Text = String.Format("{0}:{1}:{2}", h.ToString().PadLeft(2, '0'), m.ToString().PadLeft(2, '0'), s.ToString().PadLeft(2, '0'));
+            tDeath.Start();
+            if (h == 1)
+            {
+
+                tDeath.Stop();
+            }
+        }
+
+        private void btAimLabs_Click(object sender, EventArgs e)
+        {
+            aimClick = true;
+            deathClick = false;
+            tDeath.Stop();
+            studyClick = false;
+            tStudy.Stop();
+            breakClick = false;
+            tBreak.Stop();
+
+            h = 0;
+            m = 0;
+            s = 0;
+            chooice = "btnAimLab";
+            tAim.Start();
+            if(h ==  1)
+            {
+                tAim.Stop();
+            }
+        }
+
+        private void btStudy_Click(object sender, EventArgs e)
+        {
+
+            studyClick = true;
+            breakClick = false;
+            tBreak.Stop();
+            deathClick = false;
+            tDeath.Stop();
+            aimClick = false;
+            tAim.Stop();
+
+            h = 0;
+            m = 0;
+            s = 0;
+            chooice = "btnStudy";
+            tStudy.Start();
+            if (h == 1)
+            {
+                tStudy.Stop();
+            }
+
+        }
+
+        private void btBreak_Click(object sender, EventArgs e)
+        {
+            breakClick = true;
+            studyClick = false;
+            tStudy.Stop();
+            deathClick = false;
+            tDeath.Stop();
+            aimClick = false;
+            tAim.Stop();
+            h = 0;
+            m = 0;
+            s = 0;
+            
+
+
+            chooice = "btnBreak.Text";
+            tBreak.AutoReset = true;
+            tBreak.Start();
+            if ( s == 10)
+            {
+                tBreak.Stop();
+            }
+        }
+
+        private void frmNeloFoceser_Load(object sender, EventArgs e)
+        {
+            int timeInterval = 1000;
+            tDeath = new System.Timers.Timer();
+            tDeath.Interval = timeInterval;
+            tDeath.Elapsed += OnTimeEventDeath;
+
+            tAim = new System.Timers.Timer();
+            tAim.Interval = timeInterval;
+            tAim.Elapsed += OnTimeEventAim;
+
+            tStudy = new System.Timers.Timer();
+            tStudy.Interval = timeInterval;
+            tStudy.Elapsed += OnTimeEventStudy;
+
+            tBreak = new System.Timers.Timer();
+            tBreak.Interval = timeInterval;
+            tBreak.Elapsed += OnTimeEventBreak;
+
+        }
+
+        private void OnTimeEventDeath(object sender, ElapsedEventArgs e)
+        {
+            StopSimpleSound();
+            Invoke(new Action(() =>
+            {
+                s += 1;
+                if(s== 60)
+                {
+                    s = 0;
+                    m  += 1;
+                }
+                if (m == 60)
+                {
+                    m = 0;
+                    h += 1;
+                }
+                if (h == 1)
+                {
+                    playSimpleSound();
+                    deathClick = false;
+                }
+
+                if(deathClick == true)
+                {
+                    btnDeathMatch.Text = String.Format("{0}:{1}:{2}", h.ToString().PadLeft(2, '0'), m.ToString().PadLeft(2, '0'), s.ToString().PadLeft(2, '0'));
+                }
+              
+                
+                
+                
+                
+            }));
+           
+
+        }
+
+        //Aim event timer
+        private void OnTimeEventAim(object sender, ElapsedEventArgs e)
+        {
+            StopSimpleSound();
+            Invoke(new Action(() =>
+            {
+                s += 1;
+                if (s == 60)
+                {
+                    s = 0;
+                    m += 1;
+                }
+                if (m == 60)
+                {
+                    m = 0;
+                    h += 1;
+                }
+                if (h == 1)
+                {
+                    aimClick = false;
+                    playSimpleSound();
+                }
+
+              
+                if (aimClick == true)
+                {
+                    btnAimLab.Text = String.Format("{0}:{1}:{2}", h.ToString().PadLeft(2, '0'), m.ToString().PadLeft(2, '0'), s.ToString().PadLeft(2, '0'));
+
+                }
+              
+
+
+
+            }));
+
+
+        }
+        // Study event timer
+        private void OnTimeEventStudy(object sender, ElapsedEventArgs e)
+        {
+            StopSimpleSound();
+            Invoke(new Action(() =>
+            {
+                s += 1;
+                if (s == 60)
+                {
+                    s = 0;
+                    m += 1;
+                }
+                if (m == 60)
+                {
+                    m = 0;
+                    h += 1;
+                }
+                if (h == 1)
+                {
+                    studyClick = false;
+                    playSimpleSound();
+                }
+
+              
+                if (studyClick == true)
+                {
+                    btnStudy.Text = String.Format("{0}:{1}:{2}", h.ToString().PadLeft(2, '0'), m.ToString().PadLeft(2, '0'), s.ToString().PadLeft(2, '0'));
+                }
+               
+
+
+
+            }));
+
+
+        }
+        //Break event timer
+        private void OnTimeEventBreak(object sender, ElapsedEventArgs e)
+        {
+            StopSimpleSound();
+
+            Invoke(new Action(() =>
+            {
+                s += 1;
+                if (s == 60)
+                {
+                    s = 0;
+                    m += 1;
+                }
+                if (m == 60)
+                {
+                    m = 0;
+                    h += 1;
+                }
+                if (m == 11)
+                {
+                    tAim.Stop();
+                    breakClick = false;
+                    playSimpleSound();
+
+                }
+                if (breakClick == true)
+                {
+                    btnBreak.Text = String.Format("{0}:{1}:{2}", h.ToString().PadLeft(2, '0'), m.ToString().PadLeft(2, '0'), s.ToString().PadLeft(2, '0'));
+
+                }
+
+
+
+
+            }));
+        }//End of method
+
+        private static void playSimpleSound()
+        {
+            SoundPlayer simpleSound = new SoundPlayer(@"C:\Users\STRE6T\Music\Black.wav");
+            simpleSound.Play();
+        }//end of method
+
+        private static void StopSimpleSound()
+        {
+            SoundPlayer simpleSound = new SoundPlayer(@"C:\Users\STRE6T\Music\Black.wav");
+            simpleSound.Stop();
+           
+        }//end of method
+    }
+}
